0f075b0efe929d373b08aeaf3c3acf591793fa42
Gabriel Gonzatti <109048186+GabrielGonzatti@users.noreply.github.com>
2024-07-25 17:51:26 -0300

    Atualizando Javascript

---
+++ Udemy/Javascript/Estruturas/script.js
@@ -0,0 +1,9 @@
+// 1 - variáveis
+let nome = "Matheus";
+
+console.log(nome);
+
+
+let numero = prompt("Digite um número: ");
+
+alert("Ola")
---
+++ Udemy/Javascript/Intro/script.js
@@ -0,0 +1,8 @@
+console.log("Teste")
+
+// Tipos de dados:
+// String
+// Number
+// Boolean
+// Undefined/null
+//object
